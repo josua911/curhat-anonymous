35154fa0562d85f9a5e0ce709ea4da1e9b76d852
josua911 <josuaskill911@gmail.com>
2022-04-24 09:36:36 +0700

    Initial Commit
    Version 1.0

---
+++ navbar.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid warna-background">
+        <img src="image/logo.png" alt="" class="img-fluid" style="width: 5%;">
+        <a class="navbar-brand text-light" href="halaman_utama.php">Curhat Anonymous</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active text-light" href="halaman_utama.php">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active text-light" href="profile.php">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active text-light" href="buat_post.php">Buat Post</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active text-light" href="operasi_get.php?submit=logout">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<style>
+    .warna-background {
+        background-color: #4168df;
+    }
+</style>
---
+++ register.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Register</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+</head>
+
+<body>
+
+    <body>
+        <div class="container tengah">
+            <div class="card mx-auto kartu" style="width: 18rem">
+                <div class="card-body">
+                    <div class="text-center my-3">
+                        <img src="image/logo.png" alt="" class="img-fluid gambar" />
+                    </div>
+                    <form action="operasi.php" method="POST">
+                        <div class="input-container">
+                            <i class="fa fa-user icon my-auto"></i>
+                            <input class="input-field" type="text" placeholder="Username" name="username" required />
+                        </div>
+                        <div class="input-container">
+                            <i class="fa fa-key icon my-auto"></i>
+                            <input class="input-field" type="password" placeholder="Password" name="password" required />
+                        </div>
+                        <div class="input-container">
+                            <i class="fa fa-envelope icon my-auto"></i>
+                            <input class="input-field" type="email" placeholder="Email" name="email" required />
+                        </div>
+                        <div class="text-center mt-5">
+                            <button class="btn btn-light huruf-login" type="submit" name="submit" value="register">REGISTER</button>
+                        </div>
+                    </form>
+                    <div class="text-end">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    </body>
+
+</html>
+
+<style>
+    body {
+        background-color: #4168df;
+    }
+
+    .tengah {
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .gambar {
+        width: 50%;
+        height: 50%;
+    }
+
+    .kartu {
+        background-color: #4168df;
+        border: 0px;
+    }
+
+    .huruf-login {
+        color: #4168df;
+        font-weight: 600;
+        width: 100%;
+    }
+
+    .no-spacing {
+        margin: 0;
+    }
+
+    .input-container {
+        display: -ms-flexbox;
+        display: flex;
+        width: 100%;
+        margin-bottom: 15px;
+        border: 1px solid white;
+        border-radius: 15px;
+    }
+
+    .icon {
+        background: #4168df;
+        color: white;
+        min-width: 50px;
+        text-align: center;
+        min-height: 100%;
+    }
+
+    .input-field,
+    .input-field:active,
+    .input-field:focus {
+        height: 50px;
+        background-color: #4168df;
+        outline: none;
+        border-width: 0px;
+        border: none;
+        color: white;
+    }
+
+    .input-field::placeholder {
+        color: white;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    a:hover {
+        text-decoration: underline;
+        text-decoration-color: white;
+    }
+</style>
